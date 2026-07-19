95f524ecd34e988c4b77b710c471a31cc66c3c54
xdziuba <xdziuba@gmail.com>
2026-07-19 11:08:56 +0200

    chore(release): wersja 1.0.0 + wpis whatsnew (lokalny build)

--- src/renderer/panels/app-meta.ts
+++ src/renderer/panels/app-meta.ts
@@ -5,7 +5,7 @@
  * trzymamy ją tu jako stałą i podbijamy razem z wydaniami.
  */
 
-export const APP_VERSION = '0.9.0';
+export const APP_VERSION = '1.0.0';
 export const APP_NAME = 'LumaShell';
 export const GITHUB_REPO = 'xdziuba/LumaShell';
 export const GITHUB_URL = `https://github.com/${GITHUB_REPO}`;
